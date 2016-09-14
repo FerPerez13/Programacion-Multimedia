588eda18a497591798086238f36856a586f35e6c
FerPerez13 <dam.fperez@institutoserpis.org>
2016-09-14 09:35:26 +0200

    Primeros proyectos

---
+++ HolaMundo2/app/src/main/java/com/example/mati/holamundo2/Pantalla2.java
@@ -0,0 +1,32 @@
+package com.example.mati.holamundo2;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class Pantalla2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pantalla2);
+
+        final TextView otroSaludo = (TextView) findViewById(R.id.miMensaje);
+
+        Bundle miBundleRecoger = getIntent().getExtras();
+        otroSaludo.setText(miBundleRecoger.getString("TEXTO"));
+
+        final Button miBotonVolver = (Button) findViewById(R.id.botonVolver);
+        miBotonVolver.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent miIntent = new Intent(Pantalla2.this, MainActivity.class);
+                startActivity(miIntent);
+
+            }
+        });
+        }
+}
