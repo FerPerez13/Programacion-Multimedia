40ba5ba56df0f62e3ef740ffa528fb450ea493b9
FerPerez13 <dam.fperez@institutoserpis.org>
2016-10-24 12:31:44 +0200

    Finalizado el ejercicio ListaObjeto

--- ListaObjeto/app/src/main/java/com/example/mati/listaobjeto/MainActivity.java
+++ ListaObjeto/app/src/main/java/com/example/mati/listaobjeto/MainActivity.java
@@ -17,12 +17,9 @@ import android.widget.Toast;
 public class MainActivity extends AppCompatActivity {
 
     private Titular[] datos = new Titular[]{
-            new Titular("Titulo 1", "Subtitulo largo 1"),
-            new Titular("Titulo 2", "Subtitulo largo 2"),
-            new Titular("Titulo 3", "Subtitulo largo 3")
-        /*new Titular("Titulo 1", "Subtitulo largo 1", R.drawable.img1),
-        new Titular("Titulo 2", "Subtitulo largo 2", R.drawable.img2),
-        new Titular("Titulo 3", "Subtitulo largo 3", R.drawable.img1)*/
+            new Titular("Titulo 1", "Subtitulo largo 1", R.drawable.img1),
+            new Titular("Titulo 2", "Subtitulo largo 2", R.drawable.img2),
+            new Titular("Titulo 3", "Subtitulo largo 3", R.drawable.img1)
     };
 
     @Override
@@ -31,44 +28,75 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         AdaptadorTitulares adaptador = new AdaptadorTitulares(this);
-        ListView lstOpciones = (ListView)findViewById(R.id.LstOpciones);
+        ListView lstOpciones = (ListView) findViewById(R.id.LstOpciones);
         lstOpciones.setAdapter(adaptador);
 
         lstOpciones.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView arg0, View arg1, int position, long id) {
-                String mensaje = "Titulo: "+datos[position].getTitulo()+" Subtitulo: "+datos[position].getSubtitulo();
-                Toast.makeText(getApplicationContext(),mensaje,Toast.LENGTH_LONG).show();
+                String mensaje = "Titulo: " + datos[position].getTitulo() + " Subtitulo: " + datos[position].getSubtitulo();
+                Toast.makeText(getApplicationContext(), mensaje, Toast.LENGTH_LONG).show();
+            }
+
+            public void onNothingSelected(AdapterView<?> adapterView) {
             }
         });
 
     }
 
+    static class ViewHolder {
+        TextView titulo;
+        TextView subtitulo;
+        ImageView imagen;
+    }
 
     class AdaptadorTitulares extends ArrayAdapter {
         Activity context;
 
-        AdaptadorTitulares(Activity context){
+        AdaptadorTitulares(Activity context) {
             super(context, R.layout.listitem_titular, datos);
-            this.context=context;
+            this.context = context;
         }
 
-        public View getView(int i, View convertView, ViewGroup parent){
+        public View getView(int i, View convertView, ViewGroup parent) {
+            /* Vamos a mejorar el código más abajo
+
             LayoutInflater inflater = context.getLayoutInflater();
             View item = inflater.inflate(R.layout.listitem_titular,null);
 
-            TextView lblTitulo = (TextView)findViewById(R.id.tvTitulo);
+            TextView lblTitulo = (TextView)item.findViewById(R.id.tvTitulo);
             lblTitulo.setText(datos[i].getTitulo());
 
-            TextView lblSubtitulo = (TextView)findViewById(R.id.tvSubtitulo);
+            TextView lblSubtitulo = (TextView)item.findViewById(R.id.tvSubtitulo);
             lblSubtitulo.setText(datos[i].getSubtitulo());
 
-            /*ImageView imagen = (ImageView)findViewById(R.id.ivImagen);
-            imagen.setBackground(getDrawable(datos[i].getImagen()));*/
+            ImageView imagen = (ImageView)item.findViewById(R.id.ivImagen);
+            imagen.setBackground(getDrawable(datos[i].getImagen()));
 
-            return item;
+            return item;*/
+
+            View item = convertView;
+            ViewHolder holder;
+
+            if (item == null) {
+                LayoutInflater inflater = context.getLayoutInflater();
+                item = inflater.inflate(R.layout.listitem_titular, null);
+
+                holder = new ViewHolder();
+                holder.titulo = (TextView) item.findViewById(R.id.tvTitulo);
+                holder.subtitulo = (TextView) item.findViewById(R.id.tvSubtitulo);
+                holder.imagen = (ImageView)item.findViewById(R.id.ivImagen);
+                
+                item.setTag(holder);
+            } else {
+                holder = (ViewHolder) item.getTag();
+            }
+                holder.titulo.setText(datos[i].getTitulo());
+                holder.subtitulo.setText(datos[i].getSubtitulo());
+                holder.imagen.setBackground(getDrawable(datos[i].getImagen()));
+                return (item);
         }
 
     }
-
 }
+
--- ListaObjeto/app/src/main/java/com/example/mati/listaobjeto/Titular.java
+++ ListaObjeto/app/src/main/java/com/example/mati/listaobjeto/Titular.java
@@ -1,5 +1,7 @@
 package com.example.mati.listaobjeto;
 
+import android.media.Image;
+
 /**
  * Created by mati on 19/10/16.
  */
@@ -7,23 +9,27 @@ public class Titular {
 
     private String titulo;
     private String subtitulo;
+    private int imagen;
 
-    public Titular(String subtitulo, String titulo) {
+    public Titular(String subtitulo, String titulo, int imagen) {
         this.subtitulo = subtitulo;
         this.titulo = titulo;
+        this.imagen = imagen;
     }
 
     public String getSubtitulo() {
         return subtitulo;
     }
 
+    public void setImagen(int imagen) { this.imagen = imagen; }
+
+    public int getImagen() { return imagen;}
+
     public String getTitulo() {
         return titulo;
     }
 
-    public void setTitulo(String titulo) {
-        this.titulo = titulo;
-    }
+    public void setTitulo(String titulo) { this.titulo = titulo; }
 
     public void setSubtitulo(String subtitulo) {
         this.subtitulo = subtitulo;
